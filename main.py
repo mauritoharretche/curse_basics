057aa197524578e0ad3e8a56d788b72ccba3fba4
Mau <mauroharretche@gmail.com>
2025-08-05 11:32:32 -0300

    ...

--- main.py
+++ main.py
@@ -7,8 +7,42 @@ edad = 28
 altura = 1.65
 pais = 'Arg'
 
-# acceder a la variables
-
+#valores inicales 
+print('Valores iniciales')
 print('Edad:', edad )
 print('Altura:', altura)
 print('Pais:', pais)
+
+
+# acceder a la variables
+
+# valores modificados
+
+edad = 122
+altura = 1.90
+pais = 'USA'
+
+print('valores modificados')
+print('Edad:', edad )
+print('Altura:', altura)
+print('Pais:', pais)
+
+# modificar variables
+
+# reglas y convenciones en variables en python
+
+print('Hello World')
+
+name = "vivian"
+age = 15
+height = 5.8
+is_gamer = True
+
+print('Name:', name)
+print('age:', age)
+print('height:', height)
+print('is gamer:', is_gamer)
+
+# talking to the user input()
+user_name = input('whats your name?')
+print("welcome!, " + user_name + "!")
