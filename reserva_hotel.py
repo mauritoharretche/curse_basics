b8b0ea6c0a98fad9e0e8d8d12d02004216da6376
Mau <mauroharretche@gmail.com>
2025-08-06 13:16:38 -0300

    update

---
+++ reserva_hotel.py
@@ -0,0 +1,22 @@
+print('*** Sistema de reserva de hoteles ***')
+
+nombre = 'mau'
+dias = 12
+tarifa = 1290
+vista_al_mar = True
+
+print('nombre', nombre)
+print('dias', dias)
+print('tarifa', tarifa)
+print('vista al mar?', vista_al_mar)
+
+
+#modificado
+dias= 10
+tarifa = 500
+vista_al_mar = False
+
+print('nombre', nombre)
+print('dias', dias)
+print('tarifa', tarifa)
+print('vista al mar?', vista_al_mar)
---
+++ tipos_datos.py
@@ -0,0 +1,23 @@
+#ejemplo de tipos de datos 
+
+edad = 23
+print('edad:', edad)
+
+altura = 1.76
+print('altura:', altura)
+
+#string
+
+el_nombre = 'ana'
+print('nombre:', el_nombre)
+
+#boolean
+
+es_estudiante = True
+print('es estudiante? ', es_estudiante)
+
+#none ausencia de valor
+
+direccion = None
+
+print('direccion:', direccion)
